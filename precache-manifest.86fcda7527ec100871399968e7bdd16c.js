fd768849520f71f613f83b876c928150716e7628
Aleksey Grygorenko <aleksey2892@gmail.com>
2020-10-07 18:13:40 +0300

    Updates

---
+++ precache-manifest.86fcda7527ec100871399968e7bdd16c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "975b6138e5fb29fab542f1665e91b9c2",
+    "url": "/goit-react-hw-06-phonebook/index.html"
+  },
+  {
+    "revision": "586cc271e500f8e0a9ef",
+    "url": "/goit-react-hw-06-phonebook/static/css/2.3d72dcb8.chunk.css"
+  },
+  {
+    "revision": "262d3e0118c8971338ca",
+    "url": "/goit-react-hw-06-phonebook/static/css/main.b5b40437.chunk.css"
+  },
+  {
+    "revision": "586cc271e500f8e0a9ef",
+    "url": "/goit-react-hw-06-phonebook/static/js/2.ede2339f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/goit-react-hw-06-phonebook/static/js/2.ede2339f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "262d3e0118c8971338ca",
+    "url": "/goit-react-hw-06-phonebook/static/js/main.cd3e8e26.chunk.js"
+  },
+  {
+    "revision": "9d1a26fb56dbe9d98a08",
+    "url": "/goit-react-hw-06-phonebook/static/js/runtime-main.b0a8c527.js"
+  }
+]);
